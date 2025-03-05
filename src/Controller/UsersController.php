ce1a06985f6114616b68b7b09addaf47bb55dc0c
VictorBaleroni <victorcostabaleroni0@gmail.com>
2025-03-05 17:41:15 -0300

    login page

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -8,10 +8,9 @@ class UsersController extends AppController{
 
     public function beforeFilter(\Cake\Event\EventInterface $event){
         parent::beforeFilter($event);
-
         $this->Authentication->allowUnauthenticated(['login', 'logout']);
     }
-    
+
     public function login(){
         $this->request->allowMethod(['get', 'post']);
         $result = $this->Authentication->getResult();
@@ -22,7 +21,6 @@ class UsersController extends AppController{
                 return $this->redirect($redirect);
             }
         }
-
         // Display error if user submitted and authentication failed
         if ($this->request->is('post') && !$result->isValid()) {
             $this->Flash->error(__('Invalid username or password'));
