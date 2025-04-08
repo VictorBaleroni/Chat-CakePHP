2b9a521868ab7dcf557358df253f05fc3ce815c0
VictorBaleroni <victorcostabaleroni0@gmail.com>
2025-04-08 13:00:56 -0300

    list user init

--- src/WebSocket/ChatServer.php
+++ src/WebSocket/ChatServer.php
@@ -17,7 +17,6 @@ class ChatServer implements MessageComponentInterface{
     {
         $this->clients->attach($conn);
        
-
         $uristring = $conn->httpRequest->getUri()->getQuery();
 
         parse_str($uristring, $uriarray);
@@ -34,8 +33,7 @@ class ChatServer implements MessageComponentInterface{
             $send_data['id'] = $users_id;
 
             foreach($this->clients as $client){
-                    if($client->resourceId != $conn->resourceId)
-                    {
+                    if($client->resourceId != $conn->resourceId){
                         $client->send(json_encode($send_data));
                     }
                 }
@@ -47,6 +45,44 @@ class ChatServer implements MessageComponentInterface{
     public function onMessage(ConnectionInterface $conn, $msg){
         $data = json_decode($msg);
         
+    if(isset($data->type)){
+        if($data->type == 'request_users_list'){
+            $usersTable = TableRegistry::getTableLocator()->get('Users');
+
+            $userData = $usersTable->find()
+                ->select(['id', 'name'])
+                ->where(['id !=' => $data->me_user_id])
+                ->orderByAsc('name')
+                ->all();
+
+            $item_data = array();
+
+            foreach($userData as $ud){
+                $item_data[] = array(
+                    'name' => $ud['name'],
+                    'id' => $ud['id']
+                );
+            }
+
+            $sender_user_conn = $usersTable->find()->select('conn_id')->where(['id IN' => $data->me_user_id])->all();
+
+            $response_data['data_user'] = $item_data;
+
+            $response_data['response_load_users'] = true;
+
+            foreach($this->clients as $client)
+                {
+                    if($client->resourceId == $sender_user_conn[0]->conn_id)
+                    {
+                        $client->send(json_encode($response_data));
+                    }
+                }
+        }
+
+        if($data->type == 'request_search_user'){
+            
+        }
+    }
     }
 
     public function onClose(ConnectionInterface $conn)
